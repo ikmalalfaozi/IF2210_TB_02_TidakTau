4b0bd85b1454c851ab7327bb6c26f837010f66d8
rkvilena <rwad0711@gmail.com>
2022-03-14 11:29:43 +0700

    Edit inventory structure

--- src/Inventory/Inventory.cpp
+++ src/Inventory/Inventory.cpp
@@ -1,25 +1,44 @@
+// -----------Inventory implementation-------------
 #include "Inventory.hpp"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-Inventory::Inventory(){}
+Inventory::Inventory(){
+    this->activesize = 0;
+    this->itemlist = new InventorySlot[CAPACITY];
+    for (int i = 0; i < CAPACITY; i++){
+        string slotIDgenerate = "I" + i;
+        this->itemlist[i].set_slotID(slotIDgenerate);
+    }
+}
 
 Inventory::Inventory(Inventory& other){
-    for (int i = 0; i < sizeof(other); i++){
-        for (int j = 0; j < sizeof(other); j++){
-            this->itemchest[i][j] = other.itemchest[i][j];
-        }
+    this->activesize = other.activesize;
+    this->itemlist = new InventorySlot[CAPACITY];
+    for (int i = 0; i < this->activesize; i++){
+        this->itemlist[i] = other.itemlist[i];
+    }
+    for (int i = this->activesize; i < CAPACITY; i++){
+        string slotIDgenerate = "I" + i;
+        this->itemlist[i].set_slotID(slotIDgenerate);
     }
 }
 
 Inventory::~Inventory(){
-    delete this->itemchest;
+    delete[] this->itemlist;
 }
 
-void Inventory::addItem(int item, int quantity){
-
+void Inventory::addItem(int itemID, int quantity){
+    bool foundempty = false;
+    int idx = 0;
+    do{
+        idx++;
+    }while(!foundempty);
+    // Belum bisa dilanjut
+    // Cek jenis item melalui parameter item
+    // Parameter yang diminta -> Item
 }
 
 void Inventory::deleteItem(int slotID, int quantity){
@@ -32,4 +51,36 @@ void Inventory::stackItem(int slotIDsrc, int slotIDdest){
 
 void Inventory::useItem(int slotID){
     
+}
+
+// -----------InventorySlot implementation-------------
+
+InventorySlot::InventorySlot(){
+    this->slotID = "II";
+    this->itemID = -999;
+    this->quantity = 0;
+}
+
+void InventorySlot::set_slotID(string slotid){
+    this->slotID = slotid;
+}
+
+void InventorySlot::set_itemID(int itemid){
+    this->itemID = itemid;
+}
+
+void InventorySlot::set_quantity(int quantity){
+    this->quantity = quantity;
+}
+
+string InventorySlot::get_slotID() const{
+    return this->slotID;
+}
+
+int InventorySlot::get_itemID() const{
+    return this->itemID;
+}
+
+int InventorySlot::get_quantity() const{
+    return this->quantity;
 }
--- src/Inventory/Inventory.hpp
+++ src/Inventory/Inventory.hpp
@@ -1,29 +1,42 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
+#define CAPACITY 27
 #include <string>
 
 // BELUM SELESAI
 
 class Inventory {
 private:
-    InventorySlot itemchest[3][9];
+    int activesize;
+    InventorySlot* itemlist;
 public:
     Inventory();
     Inventory(Inventory&);
     ~Inventory();
-    void addItem(int item, int quantity);// Menambah item ke inventory
+    void addItem(int itemID, int quantity);// Menambah item ke inventory
     void deleteItem(int slotID, int quantity); // Membuang item dari inventory
     void stackItem(int slotIDsrc, int slotIDdest); // Menumpuk item non tool yang sama pada inventory
     void useItem(int slotID); // Menggunakan item
 };
 
 // Tahap perencanaan
-class InventorySlot : public Inventory {
+class InventorySlot {
     private:
-        // Karnea kelas item belum ada, 
+        // Karena kelas item belum ada, 
         // sementara pakai tipe data integer
-        int item;
         string slotID;
+        int itemID;
+        int quantity;
+    public:
+        InventorySlot();
+
+        void set_slotID(string slotid);
+        void set_itemID(int itemid);
+        void set_quantity(int quantity);
+
+        string get_slotID() const;
+        int get_itemID() const;
+        int get_quantity() const;
 };
 
 #endif
