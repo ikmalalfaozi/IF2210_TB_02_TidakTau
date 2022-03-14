ec234d6613a789f475eb15046de15106b4603592
eiffelaqila <16520324@std.stei.itb.ac.id>
2022-03-14 09:40:39 +0700

    Rancangan Show - Export

---
+++ src/Command/Export.hpp
@@ -0,0 +1,24 @@
+#ifndef EXPORT_H
+#define EXPORT_H
+
+#include <string>
+using namespace std;
+
+/* Include .hpp / .h */
+
+
+class Export {
+private:
+    /* Exported Attribute */
+
+
+public:
+    /* CONSTRUCTOR */
+    Export();
+
+    /* EXPORT METHOD */
+    void export(/* Inventory inv */);
+};
+
+#endif // EXPORT_H
+
---
+++ src/Command/Show.hpp
@@ -0,0 +1,23 @@
+#ifndef SHOW_H
+#define SHOW_H
+
+#include <string>
+using namespace std;
+
+/* Include .hpp / .h */
+
+
+class Show {
+private:
+    /* Showed Attribute */
+
+
+public:
+    /* CONSTRUCTOR */
+    Show();
+
+    /* EXPORT METHOD */
+    void Show(/* Inventory inv */);
+};
+
+#endif // SHOW_H
